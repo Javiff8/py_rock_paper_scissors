7232f4c080e5c0623ff98b2602daeedeed5f135c
Javiff8 <javi.ff.978@gmail.com>
2022-11-08 00:09:25 +0100

    refactorized game: the code has been divided in smaller functions for better readability

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import random
 
-options = ("rock", "scissors", "paper")
-print("Welcome to rock, scissors papers, the classic game. You may now choose between the following options (please type your imput, you may stop the game by typing 'end' without quotes).")
+def main():
+	print("Welcome to rock, scissors papers, the classic game. You may now choose between the following options (please type your imput, you may stop the game by typing 'end' without quotes).")
+	run_game()
 
-user_wins = 0
-pc_wins = 0
-rounds = 1
-
-while True:
-	print("*" * 10)
-	print("ROUND", rounds)
-	print("*" * 10)
-	print("Your wins -->", user_wins)
-	print("Computer wins -->", pc_wins)
-
+def choose_options():
+	options = ("rock", "scissors", "paper")
 	user = input("Rock, paper or scissors? --> ").lower()
 
 	if user == "end":
 		print("The game has been aborted. Quitting means you lost!")
-		break
+		exit()
 
 	if not user in options:
 		print("The option you chose is not rock, paper nor scissors, please select a valid option.")
-		continue
+		return None, None
 
-	rounds += 1
 	pc = random.choice(options)
 	print("User choice:", user.capitalize())
 	print("Computer choice:", pc.capitalize())
+	return user, pc
 
+def print_next_round(user_wins, pc_wins, rounds):
+	print("*" * 20)
+	print("ROUND", rounds)
+	print("*" * 20)
+	print("Your wins -->", user_wins)
+	print("Computer wins -->", pc_wins)
+
+def check_winner(user, user_wins, pc, pc_wins):
 	if user == pc:
 		print("Tie!")
 	elif user == "scissors":
@@ -49,7 +49,7 @@ while True:
 			print("Rock wins scissors")
 			print("You win this round!")
 			user_wins += 1
-	elif user == "paper":
+	else:
 		if pc == "scissors":
 			print ("Scissors win paper")
 			print("You lose this round!")
@@ -58,14 +58,35 @@ while True:
 			print("Paper wins rock")
 			print("You win this round!")
 			user_wins += 1
+	return user_wins, pc_wins
 
+def who_wins(user_wins, pc_wins):
 	if user_wins ==  2:
-		print("*" * 18)
+		print("*" * 20)
 		print("You won the game!")
-		print("*" * 18)
-		break
+		print("*" * 20)
+		exit()
 	elif pc_wins ==  2:
-		print("*" * 18)
+		print("*" * 20)
 		print("You lost the game!")
-		print("*" * 18)
-		break
+		print("*" * 20)
+		exit()
+
+def run_game():
+	user_wins = 0
+	pc_wins = 0
+	rounds = 1
+
+	while True:
+		print_next_round(user_wins, pc_wins, rounds)
+		user, pc = choose_options()
+
+		if user == None:
+			rounds += 1
+			continue
+
+		user_wins, pc_wins = check_winner(user, user_wins, pc, pc_wins)
+		who_wins(user_wins, pc_wins)
+		rounds += 1
+
+main()
